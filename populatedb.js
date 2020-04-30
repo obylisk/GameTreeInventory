110e024991a00bfe337843793c3847b46696270c
Obylisk <obylisk0gmail.com>
2020-04-30 15:15:31 +0700

    Filling out Populatedb. Not working

--- populatedb.js
+++ populatedb.js
@@ -11,9 +11,9 @@ if (!userArgs[0].startsWith('mongodb')) {
 }
 */
 var async = require('async')
-var Games = require('./models/games')
-var GameCopies = require('./models/gamecopies')
-var Categories = require('./models/categories')
+var Game = require('./models/game')
+var GameCopy = require('./models/gamecopy')
+var Category = require('./models/category')
 
 
 var mongoose = require('mongoose');
@@ -26,6 +26,20 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 var games = []
 var gamecopies = []
 var categories = []
+
+function categoryCreate(name, cb) {
+  var category = new Category({ name: name });
+
+  category.save(function (err) {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log('New Category: ' + category);
+    categories.push(category)
+    cb(null, category);
+  });
+}
 
 function gameCreate(title, description, category, price, numberOfCopies, cb) {
   gamedetail = {
@@ -49,21 +63,9 @@ function gameCreate(title, description, category, price, numberOfCopies, cb) {
   });
 }
 
-function categoryCreate(name, cb) {
-  var category = new Category({ name: name });
 
-  category.save(function (err) {
-    if (err) {
-      cb(err, null);
-      return;
-    }
-    console.log('New category: ' + category);
-    genres.push(category)
-    cb(null, category);
-  });
-}
 
-function gameCopyCreate(game, status due_back, cb) {
+function gameCopyCreate(game, status, due_back, cb) {
   gamecopydetail = {
     game: game,
   }
@@ -85,7 +87,6 @@ function gameCopyCreate(game, status due_back, cb) {
 
 function createCategories(cb) {
     async.series([
-
         function(callback) {
           categoryCreate("Party", callback);
         },
@@ -104,69 +105,21 @@ function createCategories(cb) {
 }
 
 
-function createBooks(cb) {
+function createGames(cb) {
     async.parallel([
         function(callback) {
-          bookCreate('The Name of the Wind (The Kingkiller Chronicle, #1)', 'I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.', '9781473211896', authors[0], [genres[0],], callback);
+          gameCreate('Ultimate One Night Werewolf)', 'Social deduction games such', 'Party', 50, 3, callback);
         },
-        function(callback) {
-          bookCreate("The Wise Man's Fear (The Kingkiller Chronicle, #2)", 'Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.', '9788401352836', authors[0], [genres[0],], callback);
-        },
-        function(callback) {
-          bookCreate("The Slow Regard of Silent Things (Kingkiller Chronicle)", 'Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.', '9780756411336', authors[0], [genres[0],], callback);
-        },
-        function(callback) {
-          bookCreate("Apes and Angels", "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...", '9780765379528', authors[1], [genres[1],], callback);
-        },
-        function(callback) {
-          bookCreate("Death Wave","In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...", '9780765379504', authors[1], [genres[1],], callback);
-        },
-        function(callback) {
-          bookCreate('Test Book 1', 'Summary of test book 1', 'ISBN111111', authors[4], [genres[0],genres[1]], callback);
-        },
-        function(callback) {
-          bookCreate('Test Book 2', 'Summary of test book 2', 'ISBN222222', authors[4], false, callback)
-        }
         ],
         // optional callback
         cb);
 }
 
 
-function createBookInstances(cb) {
+function createGameCopies(cb) {
     async.parallel([
         function(callback) {
-          bookInstanceCreate(books[0], 'London Gollancz, 2014.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[1], ' Gollancz, 2011.', false, 'Loaned', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[2], ' Gollancz, 2015.', false, false, callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[3], 'New York Tom Doherty Associates, 2016.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Available', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Maintenance', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[4], 'New York, NY Tom Doherty Associates, LLC, 2015.', false, 'Loaned', callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[0], 'Imprint XXX2', false, false, callback)
-        },
-        function(callback) {
-          bookInstanceCreate(books[1], 'Imprint XXX3', false, false, callback)
+          gameCopyCreate(games[0], 'Available', callback)
         }
         ],
         // Optional callback
@@ -176,9 +129,9 @@ function createBookInstances(cb) {
 
 
 async.series([
-    createGenreAuthors,
-    createBooks,
-    createBookInstances
+    createCategories,
+    createGames,
+    createGameCopies
 ],
 // Optional callback
 function(err, results) {
@@ -186,7 +139,7 @@ function(err, results) {
         console.log('FINAL ERR: '+err);
     }
     else {
-        console.log('BOOKInstances: '+bookinstances);
+        console.log('GAMECopies: '+gamecopies);
 
     }
     // All done, disconnect from database
