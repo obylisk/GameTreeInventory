9ad6a8de2bc352e426d3def0cb273e039fe1216a
Obylisk <obylisk0gmail.com>
2020-05-05 12:06:34 +0700

    Added Not Implemented sections of code

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,14 +7,14 @@ var async = require('async');
 //XXXX ADD VALIDATORS?
 
 //Display list of all Categories
-exports.category_list = function(req, res, next) {Category.find()
-.populate("category")
-.sort([["name", "ascending"]])
-.exec(function (err, list_categories) {
-  if (err) {return next(err); }
-  //Successful so render
-  res.render("category_list", {title: "Category List", category_list: list_category });
-  });
+  exports.category_list = function(req, res, next){ Category.find()
+    .populate("category")
+    .sort([["name", "ascending"]])
+    .exec(function (err, list_categories) {
+      if (err) {return next(err); }
+      //Successful so render
+      res.render("category_list", {title: "Category List", category_list: list_categories });
+    });
 };
 
 //Display detail page for a specific Category.
@@ -45,3 +45,33 @@ exports.category_detail = function(req, res, next) {
 };
 
 //XXXX Fill in CRUD code
+
+// Display Category create form on GET.
+exports.category_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category create GET');
+};
+
+// Handle Category create on POST.
+exports.category_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category create POST');
+};
+
+// Display Category delete form on GET.
+exports.category_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category delete GET');
+};
+
+// Handle Category delete on POST.
+exports.category_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category delete POST');
+};
+
+// Display Category update form on GET.
+exports.category_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category update GET');
+};
+
+// Handle Category update on POST.
+exports.category_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Category update POST');
+};
--- controllers/gameController.js
+++ controllers/gameController.js
@@ -54,3 +54,36 @@ exports.game_detail = function(req, res, next) {
       );
     });
 };
+
+
+//XXXX Fill in CRUD code
+
+// Display game create form on GET.
+exports.game_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Game create GET');
+};
+
+// Handle game create on POST.
+exports.game_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Game create POST');
+};
+
+// Display game delete form on GET.
+exports.game_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Game delete GET');
+};
+
+// Handle game delete on POST.
+exports.game_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Game delete POST');
+};
+
+// Display game update form on GET.
+exports.game_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Game update GET');
+};
+
+// Handle game update on POST.
+exports.game_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Game update POST');
+};
